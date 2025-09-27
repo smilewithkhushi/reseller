43e33a05596bdf8d2a3f8a363b1ec25efe08df6f
khushi <exotica.xo1@gmail.com>
2025-09-28 03:52:20 +0530

    pino issues resolved

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        "pino-pretty": false,
+      }
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
